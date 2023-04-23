1361dee5262e2af303371f2c8c6c8996c21d0da9
Ohtsubo <65008036+OtsuboRyuto@users.noreply.github.com>
2023-04-23 23:43:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
-c2 = boto3.client('ec2')
+import boto3
 
-response = ec2.describe_instances(
-    Filters=[        {            'Name': 'instance-state-name',            'Values': ['running']
-        }
-    ]
+pricing_client = boto3.client('pricing', region_name='us-east-1')
+
+response = pricing_client.get_products(
+    ServiceCode='AmazonEC2',
+    Filters=[
+        {'Type': 'TERM_MATCH', 'Field': 'instanceType', 'Value': 't2.micro'},
+        {'Type': 'TERM_MATCH', 'Field': 'operatingSystem', 'Value': 'Linux'}
+    ],
+    MaxResults=1
 )
 
-instances = []
+product = response['PriceList'][0]['product']
+sku = product['sku']
 
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        instances.append(instance['InstanceId'])
+price_response = pricing_client.get_products(
+    ServiceCode='AmazonEC2',
+    Skus=[sku]
+)
 
-print(instances)
+price_dimension = price_response['PriceList'][0]['terms']['OnDemand'][sku]['priceDimensions']
+
+usd_per_unit = float(price_dimension.values()[0]['pricePerUnit']['USD'])
+
+total_price = usd_per_unit * 100
+
+print(total_price)
